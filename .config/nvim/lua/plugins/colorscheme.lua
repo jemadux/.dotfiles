cebfe7e0775adb0202f63496b781c7dae21c4a6e
Klearchos-Angelos Gkountras <jemadux@cryptolab.net>
2026-03-24 13:39:01 +0200

    ⚡ lazyvim theme flexoki-dark

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,12 +1,17 @@
 return {
-  "Shatur/neovim-ayu",
-  lazy = false,
-  priority = 1000,
-  config = function()
-    require("ayu").setup({
-      mirage = false, -- false = dark, true = mirage variant
-      overrides = {},
-    })
-    vim.cmd("colorscheme ayu-dark")
-  end,
+  -- Add the flexoki plugin
+  {
+    "kepano/flexoki-neovim",
+    name = "flexoki",
+    lazy = false,    -- load at startup
+    priority = 1000, -- load before other plugins
+  },
+
+  -- Tell LazyVim to use it
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "flexoki-dark",
+    },
+  },
 }
